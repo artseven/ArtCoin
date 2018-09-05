45fa36cee6b565af1b3b67d1e2722ac1c7539f2b
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-09-04 20:03:47 -0400

    classes with methods added

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain {
+    constructor() {
+        //initializing chain with array that includes genesis block
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "09/03/2018", "Genesis block 777", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let artSevenCoin = new Blockchain();
+artSevenCoin.addBlock(new Block)
